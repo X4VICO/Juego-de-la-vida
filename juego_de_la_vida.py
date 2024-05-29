8e827df81f81aa36ef91ff0a7b9abd176ab7bd59
X4VICO <156704558+X4VICO@users.noreply.github.com>
2024-05-29 12:11:36 +0200

    Add files via upload

--- juego_de_la_vida.py
+++ juego_de_la_vida.py
@@ -1,71 +1,90 @@
-#----------------------------------------- creamos tablero
-filas = 10
-columnas = 10
+# cargamos librerias
+import pygame # usamos pygame para mostrar el juego de la vida en una ventana
+import numpy as np # usamos numpy para trabajar matrices y funciones matematicas
+import time # usamos time para que al actualizarse el tablero, de tiempo de ver la evolución del mismo
 
-# tablero vacío
-tablero = []
-for i in range(filas):
-    tablero.append([False]*columnas)
+# -------------------------- definimos ventana donde se mostrará el juego de la vida --------------------------
+
+pygame.init()
+pygame.display.set_caption("Juego de la vida by XAVICO")
+height, width = 600, 600
+screen = pygame.display.set_mode((height, width))
+bg = 32, 16, 38 # escogemos color de fondo
+screen.fill(bg) # aplicamos el color de fondo
+
+# definimos número de celdas que queremos mostrar del eje x hasta el eje y
+NumCeldasX, NumCeldasY = 35, 35
+# ajustamos el alto y ancho de las celdas según la ventana definida
+AnchoCelda = width / NumCeldasX
+AltoCelda = height / NumCeldasY
+
+#  -------------------------- estados de celda  --------------------------------------------------------------
+#                          viva = 1  // muerta = 0
+
+# damos valores aleatorios de vivos y muertos para que cada vez que se inicie el juego sea diferente
+EstadoCeldas = np.random.choice([0, 1], size=(NumCeldasX, NumCeldasY))
+
+#  -------------------------- iniciamos el bucle del juego .---------------------------------------------------
+generacion = 0
+iniciar = True
+while iniciar:
     
-#----------------------------------------- oscilador de "vida"
-# marcamos células vivas
-tablero [4] [5] = True
-tablero [5] [5] = True
-tablero [6] [5] = True
+    # implemento un contador de generaciones para tener en cuenta en que punto del juego estamos
+    # haciendo uso de la libreria numpy, añado un contador de células vivas
+    generacion += 1
+    poblacion = np.sum(EstadoCeldas)
+    print('Generación:',generacion,'|| Población:',poblacion)
 
-#----------------------------------------- mostramos tablero
-print('Estado inicial')
-for y in range(filas):
-    for x in range(columnas):
-        if tablero [y] [x]:
-            print('*', end='')
-        else:
-            print('.', end='')
-    print()
+    # copia del estado actual del juego para posteriormente rellenar las células que esten vivas 
+    NuevoEstadoCeldas = np.copy(EstadoCeldas)
+    # rellenamos de nuevo las celdas
+    screen.fill(bg)
+    time.sleep(0.2)
+
+    for y in range(0, NumCeldasX):
+        for x in range(0, NumCeldasY):
+
+            # calculamos el número de células vivas hay al rededor de cada celda
+            # aplicamos estrategia toroidal, para que los bordes sean un continuo de celdas
+            NumVecinos = EstadoCeldas[(x-1) % NumCeldasX,   (y-1) % NumCeldasY] +\
+                         EstadoCeldas [(x) % NumCeldasX,    (y-1) % NumCeldasY] +\
+                         EstadoCeldas [(x+1) % NumCeldasX,  (y-1) % NumCeldasY] +\
+                         EstadoCeldas [(x-1) % NumCeldasX,  (y) % NumCeldasY] +\
+                         EstadoCeldas [(x+1) % NumCeldasX,  (y) % NumCeldasY] +\
+                         EstadoCeldas [(x-1) % NumCeldasX,  (y+1) % NumCeldasY] +\
+                         EstadoCeldas [(x) % NumCeldasX,    (y+1) % NumCeldasY] +\
+                         EstadoCeldas [(x+1) % NumCeldasX,  (y+1) % NumCeldasY] 
+            
+            # ----------------------- reglas del juego de la vida -----------------------
+            # REGLA 1: REVIVE una célula muerta con 3 vecinos vivos
+            if EstadoCeldas[x, y] == 0 and NumVecinos == 3:
+                NuevoEstadoCeldas[x, y] = 1
+
+            # REGLA 2: MUERE una célula viva con menos de 2 o más de 3 vecinos vivos
+            elif EstadoCeldas[x, y] == 1 and (NumVecinos < 2 or NumVecinos > 3):
+                NuevoEstadoCeldas[x, y] = 0
+            # ----------------------------------------------------------------------------
+
+            # dibujamos la cuadricula haciendo uso de cordenadas para el tamaño de celda y uso de la libreria pygame para dibujar las celdas
+            poligonos = [((x)   * AnchoCelda, y * AltoCelda),
+                         ((x+1) * AnchoCelda, y * AltoCelda),
+                         ((x+1) * AnchoCelda, (y+1) * AltoCelda),
+                         ((x)   * AnchoCelda, (y+1) * AltoCelda)]
+            
+            # según el estado de la célula (viva o muerta), se rellenará de un color u otro
+            if NuevoEstadoCeldas[x, y] == 0:
+                pygame.draw.polygon(screen, (15, 6, 18), poligonos, 1)
+            else:
+                pygame.draw.polygon(screen, (0, 255, 0), poligonos, 0)
     
-#----------------------------------------- pulsos de reloj
-pulsos = 6
-# creamos nuevo tablero
-for t in range(pulsos):
-    nuevo=[]
-    for i in range(filas):
-        nuevo.append([False]*columnas)
+    #actualizamos el estado de las celdas
+    EstadoCeldas = np.copy(NuevoEstadoCeldas)
 
-#actualizamos el tablero
-for y in range(filas):
-    for x in range(columnas): # miramos la cantidad de "vida" que hay al rededor de la celda
-        n= 0
-        if y > 0 and x > 0 and tablero [y-1] [x-1]:
-            n += 1
-        if x > 0 and tablero [y] [x-1]:
-            n += 1
-        if y < filas-1 and x > 0 and tablero [y+1] [x-1]:
-            n += 1
-        if y > 0 and tablero [y-1] [x]:
-            n += 1
-        if y < filas-1 and tablero [y+1] [x]:
-            n += 1
-        if y > 0 and x < columnas-1 and tablero [y-1] [x-1]:
-            n += 1
-        if x < columnas-1 and tablero [y] [x+1]:
-            n += 1
-        if y < filas-1 and x < columnas-1 and tablero [y+1] [x+1]:
-            n +=1
-        
-        # reglas del juego
-        if tablero [y] [x] and (n == 2 or n == 3): # supervivencia
-            nuevo [y] [x] = True
-        elif not tablero [y] [x] and n == 3: # nacimiento
-            nuevo [y] [x] = True
-        else: # superpoblación y aislamiento
-            nuevo [y] [x] = False
+    # actualizamos la pantalla
+    pygame.display.flip()
 
-tablero = nuevo
-print('Pulso', t+1)
-for y in range(filas):
-    for x in range(columnas):
-        if tablero [y] [x]:
-            print('*', end='')
-        else:
-            print('.', end='')
-    print()
+    # para poder cerrar la ventana del juego de la vida
+    for salir in pygame.event.get():
+        if salir.type==pygame.QUIT:
+            iniciar = False
+pygame.quit()
